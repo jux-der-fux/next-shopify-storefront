64a3154f2ae0c6e1b3052bc1b0af488aea3ade8d
jux-der-fux <69474564+jux-der-fux@users.noreply.github.com>
2023-08-23 22:23:04 +0200

    Add eslint

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,6 +1,5 @@
 import { NextLink } from '@site/utilities/deps';
 import Image from 'next/image';
-import { StringDecoder } from 'string_decoder';
 
 const image = process.env.NEXT_PUBLIC_BRAND_LOGO;
 const repo = process.env.NEXT_PUBLIC_REPO;
@@ -15,47 +14,47 @@ export function HeroSection() {
     <section className="mx-auto max-w-2xl py-10 sm:py-20 lg:py-32">
       <div className="hidden sm:mb-8 sm:flex sm:justify-center">
         {image && (
-          <div className="w-72 h-24 rounded-lg overflow-hidden relative">
+          <div className="relative h-24 w-72 overflow-hidden rounded-lg">
             <Image src={image} alt="brand logo" fill />
           </div>
         )}
       </div>
-      <div className="text-center grid place-content-center">
+      <div className="grid place-content-center text-center">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{name} Shopify Storefront</h1>
         <p className="mt-6 text-lg leading-8 text-gray-600">
           This is a shopify template for demonstration purposes only
         </p>
-        <div className="mt-5 flex justify-between max-w-xl items-center">
-          <h3 className=" font-bold text-lg">Primary Colors</h3>
-          <div className="mt-2 flex space-x-2 justify-center items-center">
+        <div className="mt-5 flex max-w-xl items-center justify-between">
+          <h3 className=" text-lg font-bold">Primary Colors</h3>
+          <div className="mt-2 flex items-center justify-center space-x-2">
             {primaryColors &&
               Object.values(primaryColors).map((backgroundColor) => (
-                <div key={backgroundColor} className="w-5 h-5 rounded" style={{ backgroundColor }} />
+                <div key={backgroundColor} className="h-5 w-5 rounded" style={{ backgroundColor }} />
               ))}
           </div>
         </div>
-        <div className="mt-5 flex justify-between max-w-xl items-center">
-          <h3 className=" font-bold text-lg">Secondary Colors</h3>
-          <div className="mt-2 flex space-x-2 justify-center items-center">
+        <div className="mt-5 flex max-w-xl items-center justify-between">
+          <h3 className=" text-lg font-bold">Secondary Colors</h3>
+          <div className="mt-2 flex items-center justify-center space-x-2">
             {secondaryColors &&
               Object.values(secondaryColors).map((backgroundColor) => (
-                <div key={backgroundColor} className="w-5 h-5 rounded" style={{ backgroundColor }} />
+                <div key={backgroundColor} className="h-5 w-5 rounded" style={{ backgroundColor }} />
               ))}
           </div>
         </div>
-        <div className="mt-5 flex justify-between max-w-xl items-center">
-          <h3 className=" font-bold text-lg">Dark Colors</h3>
-          <div className="mt-2 flex space-x-2 justify-center items-center">
+        <div className="mt-5 flex max-w-xl items-center justify-between">
+          <h3 className=" text-lg font-bold">Dark Colors</h3>
+          <div className="mt-2 flex items-center justify-center space-x-2">
             {darkColors &&
               Object.values(darkColors).map((backgroundColor) => (
-                <div key={backgroundColor} className="w-5 h-5 rounded" style={{ backgroundColor }} />
+                <div key={backgroundColor} className="h-5 w-5 rounded" style={{ backgroundColor }} />
               ))}
           </div>
         </div>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <NextLink
             href="/products"
-            className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+            className="bg-primary-600 hover:bg-primary-500 focus-visible:outline-primary-600 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
           >
             Browse Products
           </NextLink>
